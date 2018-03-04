8b11fe77178145b1cfcbf3b8334b36544d827b9f
magic0822 <magic.fan.ys@gmail.com>
2018-03-03 21:23:08 -0500

    organize file

--- js/main.js
+++ js/main.js
@@ -3,61 +3,6 @@ var markers = [];
 var polygon = null;
 
 function initMap() {
-    //create a styles array for this map
-    var styles = [
-        {
-            featureType: 'water',
-            stylers: [
-                {color: '#19a0d8'}
-            ]
-        },
-        {
-            featureType: 'administrative',
-            elementType: 'labels.text.stroke',
-            stylers: [
-                {color: '#fff'},
-                {weight: 6}
-            ]
-        },
-        {
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {color: '#e85113'}
-            ]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [
-                {color: '#efe9e4'},
-                {lightness: -40}
-            ]
-        },
-        {
-            featureType: 'transit.station',
-            stylers: [
-                {weight: 9},
-                {hue: '#e85113'}
-            ]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'labels.icon',
-            stylers: [
-                {visibility: 'off'}
-            ]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [
-                {color: '#efe9e4'},
-                {lightness: -25}
-            ]
-        }
-    ];
-
     map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 40.7413549, lng: -73.9980244},
             zoom: 13,
@@ -65,15 +10,6 @@ function initMap() {
             mapTypeControl: false
         }
     );
-
-    var locations = [
-        {title: 'zen6', location: {lat: 40.7268239, lng: -73.9871553}},
-        {title: 'Minca', location: {lat: 40.723998, lng: -73.9851377}},
-        {title: 'Ramen Misoya', location: {lat: 40.728511, lng: -73.9903287}},
-        {title: 'Ippudo NY', location: {lat: 40.7289698, lng: -74.0011538}},
-        {title: 'Ivan Ramen', location: {lat: 40.7413779, lng: -74.0081795}},
-        {title: 'Ramen Setagaya', location: {lat: 40.7217585, lng: -73.9922562}}
-    ];
 
     var largeInfoWindow = new google.maps.InfoWindow();
 
---
+++ js/static.js
@@ -0,0 +1,62 @@
+var locations = [
+        {title: 'zen6', location: {lat: 40.7268239, lng: -73.9871553}},
+        {title: 'Minca', location: {lat: 40.723998, lng: -73.9851377}},
+        {title: 'Ramen Misoya', location: {lat: 40.728511, lng: -73.9903287}},
+        {title: 'Ippudo NY', location: {lat: 40.7289698, lng: -74.0011538}},
+        {title: 'Ivan Ramen', location: {lat: 40.7413779, lng: -74.0081795}},
+        {title: 'Ramen Setagaya', location: {lat: 40.7217585, lng: -73.9922562}}
+    ];
+
+var styles = [
+        {
+            featureType: 'water',
+            stylers: [
+                {color: '#19a0d8'}
+            ]
+        },
+        {
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [
+                {color: '#fff'},
+                {weight: 6}
+            ]
+        },
+        {
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [
+                {color: '#e85113'}
+            ]
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+                {color: '#efe9e4'},
+                {lightness: -40}
+            ]
+        },
+        {
+            featureType: 'transit.station',
+            stylers: [
+                {weight: 9},
+                {hue: '#e85113'}
+            ]
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+                {visibility: 'off'}
+            ]
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+                {color: '#efe9e4'},
+                {lightness: -25}
+            ]
+        }
+    ];
