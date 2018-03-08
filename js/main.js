874f2687e9753db627e53ab5f08fabe18739617a
magic0822 <magic.fan.ys@gmail.com>
2018-03-08 00:14:51 -0500

    revert

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 var map;
-var markers = [];
-// var polygon = null;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -10,93 +8,49 @@ function initMap() {
             mapTypeControl: false
         }
     );
+}
 
-    var largeInfoWindow = new google.maps.InfoWindow();
-    ko.applyBindings(new ViewModel());
+function ViewModel() {
+    var self = this;
+    self.initMap = new initMap();
+    window.map = self.initMap.map;
 
-    }
-
-    function ViewModel() {
-        var self = this;
-        this.locations = ko.observableArray([]);
-        locationList.forEach(function (location) {
-            self.locations.push(new Location(location))
+    self.location = function (name, lat, lng) {
+        this.name = name;
+        this.lat = ko.observable(lat);
+        this.lng = ko.observable(lng);
+        this.marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            title: name,
+            map: self.initMap.map
         });
-    }
+    };
 
-    function errorHandler() {
-        alert("Unknown error, please try again.");
-    }
+    self.locations = ko.observableArray([
+        new self.location('zen6', 40.7268239, -73.9871553),
+        new self.location('Minca', 40.723998, -73.9851377),
+        new self.location('Ramen Misoya', 40.728511, -73.9903287),
+        new self.location('Ippudo NY', 40.7289698, -74.0011538),
+        new self.location('Ivan Ramen', 40.7413779, -74.0081795),
+        new self.location('Ramen Setagaya', 40.7217585, -73.9922562)
+    ]);
 
-    //get obj
-    document.getElementById('show-listings').addEventListener('click', showListings);
-    document.getElementById('hide-listings').addEventListener('click', hideListings);
-
-    //populate the infowindow when the marker is clicked
-    function populateInfoWindow(marker, infoWindow) {
-        if (infoWindow.marker != marker) {
-            infoWindow.setContent('');
-            infoWindow.marker = marker;
-            infoWindow.addListener('closeclick', function () {
-                infoWindow.marker = null;
-            });
-            //get street view for markers
-            var streetViewService = new google.maps.StreetViewService();
-            var radius = 50;
-
-            function getStreetView(data, status) {
-                if (status == google.maps.StreetViewStatus.OK) {
-                    var nearStreetViewLocation = data.location.latLng;
-                    var heading = google.maps.geometry.spherical.computeHeading(
-                        nearStreetViewLocation, marker.position);
-                    infoWindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-                    var panoramaOptions = {
-                        position: nearStreetViewLocation,
-                        pov: {
-                            heading: heading,
-                            pitch: 30
-                        }
-                    };
-                    var panorama = new google.maps.StreetViewPanorama(
-                        document.getElementById('pano'), panoramaOptions);
-                } else {
-                    infowindow.setContent('<div>' + marker.title + '</div>' +
-                        '<div>No Street View Found</div>');
-                }
-            }
-
-            // Use streetview service to get the closest streetview image within
-            // 50 meters of the markers position
-            streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-            // Open the infowindow on the correct marker.
-            infoWindow.open(map, marker);
+    self.currentLocation = ko.observable();
+    self.locationFilter = ko.observable('');
+    self.showLocations = ko.computed(function () {
+        return ko.utils.arrayFilter(self.locations(), function (location) {
+            return location.name.toLowerCase().indexOf(self.locationFilter().toLowerCase()) !== -1;
+        });
+    }, self);
+    self.showLocations.subscribe(function(){
+        self.toggleMarkers();
+        if(self.infowindow.isOpen === true) {
+            self.infowindow.close();
+            self.infowindow.isOpen = false;
+            self.infowindowClosed();
         }
-    }
+    });
 
-    function showListings() {
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(map);
-            bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
-    }
 
-    function hideListings() {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-        }
-    }
-
-    function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-            '|40|_|%E2%80%A2',
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(10, 34),
-            new google.maps.Size(21, 34));
-        return markerImage;
-    }
 }
 
